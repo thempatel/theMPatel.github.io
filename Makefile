6147ab428079bb1dfc33ad3b32de3761049b89c5
Milan Patel <32933108+theMPatel@users.noreply.github.com>
2020-12-19 21:24:46 -0500

    remove rust for now

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
-.PHONY: all build-rust
-all: build-rust
-
-build-rust:
-	wasm-pack build ${RUST_ROOT} --out-dir ${PACKAGE_ROOT}/rust-app --target bundler
+.PHONY: all
+all:
 
 deploy:
 	${PROJECT_ROOT}/tools/deploy.sh
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-    entry: "./packages/website/src/bootstrap.js",
+    entry: "./packages/website/src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "templates/index.html")
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "rust"),
-            outDir: path.resolve(__dirname, "packages/rust-app"),
-        }),
+        })
     ],
     mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bootstrap.js",
+        filename: "app.js",
     },
 };
