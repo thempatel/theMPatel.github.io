bfaa1122656091c27e4247d49d3581ac727ba5ef
Milan Patel <32933108+theMPatel@users.noreply.github.com>
2020-12-19 22:31:00 -0500

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: "./packages/website/dist/index.js",
-    mode: "development",
+    mode: "production",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "app.js",
