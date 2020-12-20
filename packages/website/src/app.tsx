f0f341056ad4fbd87043ec0b4548b0e6c2351ba1
Milan Patel <32933108+theMPatel@users.noreply.github.com>
2020-12-19 23:01:38 -0500

    hash routing

--- packages/website/src/app.tsx
+++ packages/website/src/app.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import {
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 export const App: React.FC = () => {
-    return (
-        <span>
-            this is a test of my react app
-        </span>
-    );
+  return (
+    <>
+      <HashRouter basename="/">
+        <Switch>
+          <Route path="/home">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+        </Switch>
+        <Redirect to="/home"/>
+      </HashRouter>
+    </>
+  );
+}
+
+const Home: React.FC = () => {
+  return (
+    <span>
+      This is my homepage
+    </span>
+  );
+}
+
+const About: React.FC = () => {
+  return (
+    <span>
+      I am an engineer. Much wowe.
+    </span>
+  )
 }
--- packages/website/src/index.ts
+++ packages/website/src/index.ts
@@ -3,13 +3,13 @@ import React from 'react';
 import { App } from './app';
 
 export function init(): void {
-    const elem = document.createElement("div");
-    elem.id = 'root';
-    document.body.append(elem);
-    ReactDOM.render(
-        React.createElement(App),
-        elem,
-    );
+  const elem = document.createElement("div");
+  elem.id = 'root';
+  document.body.append(elem);
+  ReactDOM.render(
+    React.createElement(App),
+    elem,
+  );
 }
 
 init();
